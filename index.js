7dd982f932be9c6462040a26476370b493056f33
James Schultz <ninjames101@gmail.com>
2018-03-20 19:41:19 -0600

    changed my mind, removed solutions

--- index.js
+++ index.js
@@ -1,24 +1,17 @@
-console.log("hey this is working")
-console.log($ + "jQuery is working")
-$(document).ready(function(){
-    console.log('doc is ready');
-    $.get('https://quiet-bayou-99554.herokuapp.com/api/v1/contacts')
+console.log($)
 
-        .then(function(data) {
-                console.log(data.data);
-                console.log(data.data.length);
-                var i =  randomize(0,5)
-                console.log(i);
-                 $('button').click(function(){
-                     $('.name').text(data.data[i].name)
-                     $('.phone').text(data.data[i].phone)
-                     $('.message').text(data.data[i].message)
-                     $('.image-holder').attr('src', data.data[i].imageURL);                           
-        })
-      }
-    )
-})
-function randomize(min,max) {
-    return Math.floor(Math.random() * (max - min + 1) + min)
+
+$(document).ready(function(){
+    console.log('im ready')
     
-}
+    $.get('https://quiet-bayou-99554.herokuapp.com/api/v1/contacts', function (butts) {
+
+
+        
+    })
+})
+
+// function randomize(min,max) {
+//     return Math.floor(Math.random() * (max - min + 1) + min)
+    
+// }
